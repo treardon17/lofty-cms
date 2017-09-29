5eb59bd24f0b2979f93b82ced029cbfd834d95ff
Tyler Reardon <tdr.reardon@gmail.com>
2017-09-28 23:14:28 -0700

    Start frontend of CMS

---
+++ README.md
@@ -0,0 +1,13 @@
+# Lofty-CMS
+
+---
+
+## Description
+The Lofty-CMS is a headless CMS with a `graphql` API.
+
+---
+
+## Getting started
+
+1. Install `MongoDB`: `brew update` and `brew install mongodb` should do the trick if you're on a Mac
+2. Create the database folder `mkdir -p /data/db`
---
+++ front-end/src/routes.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import AppState from './state/AppState';
+// import styles
+import './resources/styles/base.scss';
+// import pages
+import Home from './pages/Home';
+
+// create global state
+const appState = new AppState();
+
+export default class Routes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      routes: [
+        <Route exact path="/" key="home" component={() => <Home state={appState} />} />,
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div id="app-container">
+          {this.state.routes}
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
